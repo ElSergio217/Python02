6badf00e1e0d0b605100479800d10236f92eb12c
Sergio Garcia <sergioa.garcia217@gmail.com>
2016-12-02 15:32:12 -0500

    Update views.py

--- django-tutorial/polls/views.py
+++ django-tutorial/polls/views.py
@@ -42,7 +42,6 @@ def vote(request, poll_id):
     try:
         selected_choice = p.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        # Redisplay the poll voting form.
         return render(request, 'polls/detail.html', {
             'poll': p,
             'error_message': "You didn't select a choice.",
@@ -50,7 +49,4 @@ def vote(request, poll_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
